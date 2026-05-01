5daee61ce611de2069c9fce2204f1631374e90eb
sri-lasya-s <ssamidala@hawk.illinoistech.edu>
2026-04-30 20:16:53 -0500

    Fix package.json for Azure deployment

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const Database = require('better-sqlite3');
-
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  dialectModule: Database,
-  storage: '/home/library.sqlite',
+  storage: process.env.NODE_ENV === 'production' ? '/home/library.sqlite' : './library.sqlite',
   logging: false,
+  dialectOptions: {
+    mode: null
+  }
 });
 
 module.exports = sequelize;
