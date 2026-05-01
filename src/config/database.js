da9754d0d968ad386a47dd4c35bbd0a09026922f
sri-lasya-s <ssamidala@hawk.illinoistech.edu>
2026-04-30 19:59:11 -0500

    Fix SQLite compatibility for Azure Linux

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
+  dialectModule: require('better-sqlite3'),
   storage: './library.sqlite',
   logging: false,
 });
